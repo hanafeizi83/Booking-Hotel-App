258d33e5d4fab2419b4acfdbfc315b152d06754c
hana feizi <hanafaizi83@gmail.com>
2025-01-20 09:23:01 +0330

    add event click to singleHotel

--- src/components/SingleHotel/SingleHotel.jsx
+++ src/components/SingleHotel/SingleHotel.jsx
@@ -2,29 +2,30 @@ import { useNavigate, useParams } from 'react-router-dom'
 import useFetch from '../../hook/useFetch'
 import { HiArrowLeft } from 'react-icons/hi';
 import { SlLocationPin } from 'react-icons/sl';
+import { useState } from 'react';
 
 function SingleHotel() {
     const { id } = useParams()
     const { data, isLoading } = useFetch(`http://localhost:5000/hotels/${id}`);
     const navigate = useNavigate()
+    const [imageId, setImageId] = useState(1)
+    const findedImage = data?.picture_url?.urls.find(item => item.id === imageId);
 
     return (
         <>
-            <button className='btn btnBack' onClick={()=>navigate(-1)}>
+            <button className='btn btnBack' onClick={() => navigate(-1)}>
                 <HiArrowLeft />
             </button>
-            {
-                data && <div className='singleHotel'>
-                    <img className='hotelImage' src={data.picture_url.url} alt={data.name} />
-                    <div className='pictureInsideHotel'>
-                        <img src={data.picture_url.img_1} alt="" />
-                        <img src={data.picture_url.img_2} alt="" />
-                        <img src={data.picture_url.img_3} alt="" />
-                        <img src={data.picture_url.img_4} alt="" />
-                    </div>
+            <div className='singleHotel'>
+                <img className='hotelImage' src={findedImage?.image} alt={data?.name} />
+                <div className='pictureInsideHotel'>
+                    {
+                        data?.picture_url?.urls.map(item => {
+                            return <img key={item.id} src={item.image} alt="" onClick={() => setImageId(item.id)} />
+                        })
+                    }
                 </div>
-
-            }
+            </div>
             <div className="SingleHotelDetail">
                 <div className="SingleHotelName">
                     <h2>{data.name}</h2>
