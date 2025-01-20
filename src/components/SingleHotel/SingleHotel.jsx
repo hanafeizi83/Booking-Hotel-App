63826bab7022fbe1f23676838c3067c0df90b0f8
hana feizi <hanafaizi83@gmail.com>
2025-01-20 10:28:34 +0330

    merge single hotel

--- src/components/SingleHotel/SingleHotel.jsx
+++ src/components/SingleHotel/SingleHotel.jsx
@@ -16,21 +16,20 @@ function SingleHotel() {
             <button className='btn btnBack' onClick={() => navigate(-1)}>
                 <HiArrowLeft />
             </button>
-            {
-                data && <div className='singleHotel'>
-                    <img className='hotelImage' src={data.picture_url.url} alt={data.name} />
-                    <div className='pictureInsideHotel'>
-                        <img src={data.picture_url.img_1} alt="" />
-                        <img src={data.picture_url.img_2} alt="" />
-                        <img src={data.picture_url.img_3} alt="" />
-                        <img src={data.picture_url.img_4} alt="" />
-                    </div>
+            <div className='singleHotel'>
+                <img className='hotelImage' src={findedImage?.image} alt={data?.name} />
+                <div className='pictureInsideHotel'>
+                    {
+                        data?.picture_url?.urls.map(item => {
+                            return <img key={item.id} src={item.image} alt="" onClick={() => setImageId(item.id)} />
+                        })
+                    }
                 </div>
             </div>
             <div className="SingleHotelDetail">
                 <div className="SingleHotelName">
                     <h2>{data.name}</h2>
-                    <p><SlLocationPin /> {data?.host_location}</p>
+                    <p><SlLocationPin /> {data.host_location}</p>
                 </div>
 
             </div>
