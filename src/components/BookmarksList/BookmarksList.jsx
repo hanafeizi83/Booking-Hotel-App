7ea4e6cc9d937839370d83d1a4b0beb3dd086e00
hana feizi <hanafaizi83@gmail.com>
2025-01-23 10:08:11 +0330

    make current bookmark and style it

--- src/components/BookmarksList/BookmarksList.jsx
+++ src/components/BookmarksList/BookmarksList.jsx
@@ -1,18 +1,17 @@
 import React from 'react'
 import ReactCountryFlag from 'react-country-flag'
 import { SlLocationPin } from 'react-icons/sl'
-import useFetch from '../../hook/useFetch'
 import { Link } from 'react-router-dom';
 import { useBookmark } from '../../context/BookmarkProvider';
 
 function BookmarksList() {
-    const { bookmarks, isLoading } = useBookmark();
+    const { bookmarks, isLoading, currentBookmark } = useBookmark();
     return (
         <div className='bookmarksList'>
             {
                 bookmarks.map(item => {
                     return <Link key={item.id} to={`${item.id}?lat=${item.latitude}&lng=${item.longitude}`} >
-                        <div className="bookmarkItem">
+                        <div className={`bookmarkItem ${currentBookmark.id === item.id ? 'currentBookmark' : ''}`}>
                             <ReactCountryFlag svg countryCode={item.countryCode} className='bookmarkFlag' style={{
                                 fontSize: '8rem',
                                 margin: '0 .8rem'
--- src/context/BookmarkProvider.jsx
+++ src/context/BookmarkProvider.jsx
@@ -1,18 +1,41 @@
-import React, { createContext, useContext } from 'react'
+import React, { createContext, useContext, useState } from 'react'
 import useFetch from '../hook/useFetch';
+import axios from 'axios';
 
 
 const BookmarkContext = createContext();
 function BookmarkProvider({ children }) {
     const { data: bookmarks, isLoading } = useFetch('http://localhost:5000/bookmarks');
+    const [currentBookmark, setCurrentBookmark] = useState(null);
+    const [isLoadingCurrBookmark, setIsLoadingCurrBookmark] = useState(false);
+
+    async function getBookmark(id) {
+        setIsLoadingCurrBookmark(true)
+        try {
+            const {data}=await axios.get(`http://localhost:5000/bookmarks/${id}`);
+            setCurrentBookmark(data);
+        } catch (error) {
+            toast.error(error?.message);
+        }finally{
+            setIsLoadingCurrBookmark(false)
+        }
+    }
+
     return (
-        <BookmarkContext.Provider value={{ bookmarks, isLoading }}>
+        <BookmarkContext.Provider value={{
+            bookmarks,
+            isLoading,
+            isLoadingCurrBookmark,
+            currentBookmark,
+            getBookmark
+        }}>
             {children}
         </BookmarkContext.Provider>
     )
 }
 
-export default BookmarkProvider
 export function useBookmark() {
     return useContext(BookmarkContext)
 }
+
+export default BookmarkProvider
