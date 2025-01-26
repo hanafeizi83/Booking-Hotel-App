e4cf8a3a37281fe04ff7bf7f9c171c92a2feb140
hana feizi <hanafaizi83@gmail.com>
2025-01-26 10:42:27 +0330

    make create new bookmark

--- src/components/AddBookmark/AddBookmark.jsx
+++ src/components/AddBookmark/AddBookmark.jsx
@@ -1,32 +1,96 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 import { BiMapAlt, BiMapPin } from 'react-icons/bi';
 import { HiArrowLeft } from 'react-icons/hi'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useSearchParams } from 'react-router-dom'
+import { useBookmark } from '../../context/BookmarkProvider';
+
+const BASE_LOCATION_URL = 'https://api-bdc.net/data/reverse-geocode-client';
+
 
 function AddBookmark() {
     const navigate = useNavigate();
+    const [searchParams, setSearchParams] = useSearchParams();
+    const lat = searchParams.get('lat');
+    const lng = searchParams.get('lng');
+    const [country, setCountry] = useState('');
+    const [cityName, setCityName] = useState('');
+    const [countryCode, setCountryCode] = useState('');
+    const [isLoadingGeoLocation, setIsLoadingGeoLocation] = useState(false)
+    const [geoLocationError, setGeoLocationError] = useState(null)
+    const { createBookmark } = useBookmark();
+    useEffect(() => {
+        async function FetchData() {
+            setIsLoadingGeoLocation(true)
+            try {
+                const { data } = await axios.get(`${BASE_LOCATION_URL}?latitude=${lat}&longitude=${lng}`)
+                if (!data.countryCode) {
+                    throw new Error(
+                        'this location is not a city . please click somewhere else !!'
+                    )
+                }
+                setCountryCode(data.countryCode);
+                setCityName(data.city || data.locality || '');
+                setCountry(data.countryCode);
+
+            } catch (error) {
+                setGeoLocationError(error?.message);
+            } finally {
+                setIsLoadingGeoLocation(false)
+            }
+        }
+        FetchData()
+    }, [lat, lng])
+
+    const handleSubmitForm = (e) => {
+        e.preventDefault();
+        if (!cityName || !country) return;
+        const newBookmark = {
+            cityName,
+            country,
+            countryCode,
+            host_location: cityName + ' ' + country,
+            latitude: lat,
+            longitude: lng
+        }
+        createBookmark(newBookmark);
+        navigate('/bookmarks')
+    }
     return (
         <>
-
             <button className='btn btnBack' onClick={() => navigate(-1)}>
                 <HiArrowLeft />
             </button>
             <div className='addBookmarkContainer'>
                 <h2 className='addBookmarkTitle'>Add New Location</h2>
-                <form>
+                <form onSubmit={handleSubmitForm}>
                     <div className="addNewBookmarkItem">
                         <label htmlFor="cityName">City Name :</label>
                         <div>
-                            <BiMapPin className='addBookmarkIcon'/>
-                            <input type="text" name='cityName' className='inputAddLocation' placeholder='enter a city name' />
+                            <BiMapPin className='addBookmarkIcon' />
+                            <input
+                                type="text"
+                                name='cityName'
+                                className='inputAddLocation'
+                                placeholder='enter a city name'
+                                value={cityName}
+                                onChange={(e) => setCityName(e.target.value)}
+                            />
                         </div>
                     </div>
 
                     <div className="addNewBookmarkItem">
                         <label htmlFor="country">Country :</label>
                         <div>
-                            <BiMapAlt className='addBookmarkIcon'/>
-                            <input type="text" name='country' className='inputAddLocation' placeholder='enter a country' />
+                            <BiMapAlt className='addBookmarkIcon' />
+                            <input
+                                type="text"
+                                name='country'
+                                className='inputAddLocation'
+                                placeholder='enter a country'
+                                value={country}
+                                onChange={(e) => setCountry(e.target.value)}
+                            />
                         </div>
                     </div>
 
--- src/context/BookmarkProvider.jsx
+++ src/context/BookmarkProvider.jsx
@@ -1,33 +1,57 @@
-import React, { createContext, useContext, useState } from 'react'
+import React, { createContext, useContext, useEffect, useState } from 'react'
 import useFetch from '../hook/useFetch';
 import axios from 'axios';
+import toast from 'react-hot-toast';
 
 
 const BookmarkContext = createContext();
 function BookmarkProvider({ children }) {
-    const { data: bookmarks, isLoading } = useFetch('http://localhost:5000/bookmarks');
+    // const { data: bookmarks, isLoading } = useFetch('http://localhost:5000/bookmarks');
     const [currentBookmark, setCurrentBookmark] = useState(null);
     const [isLoadingCurrBookmark, setIsLoadingCurrBookmark] = useState(false);
-
+    const [isLoading, setIsLoading] = useState(false)
+    const [bookmarks, setBookmarks] = useState([])
+    useEffect(() => {
+        async function FetchData() {
+            setIsLoading(true);
+            try {
+                const { data } = await axios.get('http://localhost:5000/bookmarks');
+                setBookmarks(data)
+            } catch (error) {
+                toast.error(error?.message)
+            } finally {
+                setIsLoading(false)
+            }
+        }
+        FetchData()
+    }, [])
     async function getBookmark(id) {
         setIsLoadingCurrBookmark(true)
         try {
-            const {data}=await axios.get(`http://localhost:5000/bookmarks/${id}`);
+            const { data } = await axios.get(`http://localhost:5000/bookmarks/${id}`);
             setCurrentBookmark(data);
         } catch (error) {
             toast.error(error?.message);
-        }finally{
+        } finally {
             setIsLoadingCurrBookmark(false)
         }
     }
-
+    async function createBookmark(newBookmark) {
+        try {
+            const { data } = await axios.post('http://localhost:5000/bookmarks', newBookmark);
+            setBookmarks(prev => [...prev, data])
+        } catch (error) {
+            toast.error(error?.message);
+        }
+    }
     return (
         <BookmarkContext.Provider value={{
             bookmarks,
             isLoading,
             isLoadingCurrBookmark,
             currentBookmark,
-            getBookmark
+            getBookmark,
+            createBookmark
         }}>
             {children}
         </BookmarkContext.Provider>
