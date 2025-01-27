00970e56bee42d588196302d0fda4fe3dbca58b6
hana feizi <hanafaizi83@gmail.com>
2025-01-27 10:53:35 +0330

    add useReducer to bookmark provider

--- src/context/BookmarkProvider.jsx
+++ src/context/BookmarkProvider.jsx
@@ -1,57 +1,107 @@
-import React, { createContext, useContext, useEffect, useState } from 'react'
+import React, { createContext, useContext, useEffect, useReducer, useState } from 'react'
 import useFetch from '../hook/useFetch';
 import axios from 'axios';
 import toast from 'react-hot-toast';
 
-
 const BookmarkContext = createContext();
+
+const intialState = {
+    bookmarks: [],
+    isLoading: 'false',
+    currentBookmark: null,
+    error: null
+}
+function bookmarkReducer(state, { type, payload }) {
+    switch (type) {
+        case 'Loading': return {
+            ...state,
+            isLoading: true
+        }
+        case 'bookmarks/loaded': return {
+            ...state,
+            isLoading: false,
+            bookmarks: payload
+        }
+        case 'bookmark/loaded': return {
+            ...state,
+            isLoading: false,
+            currentBookmark: payload
+        }
+        case 'bookmark/created': return {
+            ...state,
+            isLoading: false,
+            currentBookmark: payload,
+            bookmarks: [...state.bookmarks, payload]
+        }
+        case 'bookmark/deleted': return {
+            ...state,
+            isLoading: false,
+            bookmarks: state.bookmarks.filter(item => item.id !== payload)
+        }
+        case 'rejected': return {
+            ...state,
+            isLoading: false,
+            error: payload
+        }
+        default:
+            throw new Error('Unknown action')
+
+    }
+}
 function BookmarkProvider({ children }) {
-    const [currentBookmark, setCurrentBookmark] = useState(null);
-    const [isLoading, setIsLoading] = useState(false)
-    const [bookmarks, setBookmarks] = useState([])
+    const [{
+        bookmarks,
+        isLoading,
+        currentBookmark
+    }, dispatch] = useReducer(bookmarkReducer, intialState);
+
+
     useEffect(() => {
         async function FetchData() {
-            setIsLoading(true);
+            dispatch({ type: 'Loading' })
             try {
                 const { data } = await axios.get('http://localhost:5000/bookmarks');
-                setBookmarks(data)
+                dispatch({ type: 'bookmarks/loaded', payload: data })
             } catch (error) {
                 toast.error(error?.message)
-            } finally {
-                setIsLoading(false)
+                dispatch({ type: 'rejected', payload: 'an Error accurred in Loading bookmarks' })
             }
         }
         FetchData()
     }, [])
+
     async function getBookmark(id) {
-        setIsLoading(true)
+        if (id === currentBookmark?.id) return;
+        dispatch({ type: 'Loading' })
         try {
             const { data } = await axios.get(`http://localhost:5000/bookmarks/${id}`);
-            setCurrentBookmark(data);
+            dispatch({ type: 'bookmark/loaded', payload: data })
         } catch (error) {
             toast.error(error?.message);
-        } finally {
-            setIsLoading(false)
+            dispatch({ type: 'rejected', payload: 'an Error accurred in get bookmark' })
         }
     }
+
     async function createBookmark(newBookmark) {
+        dispatch({ type: 'Loading' })
         try {
             const { data } = await axios.post('http://localhost:5000/bookmarks', newBookmark);
-            setBookmarks(prev => [...prev, data])
+            dispatch({ type: 'bookmark/created', payload: data })
         } catch (error) {
             toast.error(error?.message);
+            dispatch({ type: 'rejected', payload: 'an Error accurred in create bookmark' })
         }
     }
 
     async function deleteBookmark(id) {
-        setIsLoading(true)
+        dispatch({ type: 'Loading' })
         try {
             const { data } = await axios.delete(`http://localhost:5000/bookmarks/${id}`)
-            setBookmarks(prev => prev.filter(item => item.id != id))
+            // setBookmarks(prev => prev.filter(item => item.id != id))
+            dispatch({ type: 'bookmark/deleted', payload: id })
         } catch (error) {
             toast.error(error?.message)
-        } finally {
-            setIsLoading(false)
+            dispatch({ type: 'rejected', payload: 'an Error accurred in delete bookmark' })
         }
 
     }
