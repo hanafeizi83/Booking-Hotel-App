be696b0329518f57453fb63c7427025c6215e88c
hana feizi <hanafaizi83@gmail.com>
2025-02-01 09:59:27 +0330

    responsive header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,15 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from '../../context/AuthProvider';
+import { AiOutlineMenu } from "react-icons/ai";
+import useOutsideClick from '../../hook/useOutsideClick';
 
 function Header() {
     const [small, setSmall] = useState(false);
     const { isAuthenticated, logout, user } = useAuth();
+    const [isOpen, setIsOpen] = useState(false);
     const navigate = useNavigate();
+    const responseRef = useRef();
     useEffect(() => {
         if (typeof window !== "undefined") {
             window.addEventListener("scroll", () =>
@@ -13,6 +17,7 @@ function Header() {
             );
         }
     }, []);
+    useOutsideClick(responseRef, 'menuResponsive', () => setIsOpen(false))
     const handleLogOut = () => {
         logout();
         navigate('/')
@@ -25,33 +30,43 @@ function Header() {
                         <img src="images/logo.png" alt="Hotel Logo" />
                         <h2>Serenity Hotel</h2>
                     </div>
-                    <nav className='menu'>
-                        <ul>
-                            <Link to='/'><li>Home</li></Link>
-                            <Link to='/hotels'><li>Hotels</li></Link>
-                            <Link to='/bookmarks'><li>Bookmarks</li></Link>
-                        </ul>
-                    </nav>
-                    <div className="btnAndProfile">
-                        <div className='headerBtns'>
-                            <button
-                                className='btn btnSingIn'
-                                style={{
-                                    display: isAuthenticated ? 'none' : ''
-                                }}
-                                onClick={() => navigate('/login')}
-                            >Login</button>
-                            <button
-                                className='btn btnSingUp'
-                                onClick={handleLogOut}
-                            >Log out</button>
-                        </div>
-                        {
-                            isAuthenticated && <div className="userProfile">
-                                <img src="images/user.jpg" />
-                                <p>{` Hi ${user.name}`}</p>
+                    <div className="menuResponsive" id='menuResponsive' ref={responseRef} style={{
+                        display: isOpen && 'flex'
+                    }}>
+                        <nav className='menu' style={{
+                            marginTop: isAuthenticated && isOpen && '12rem'
+                        }}>
+                            <ul>
+                                <Link to='/'><li>Home</li></Link>
+                                <Link to='/hotels'><li>Hotels</li></Link>
+                                <Link to='/bookmarks'><li>Bookmarks</li></Link>
+                            </ul>
+                        </nav>
+                        <div className="btnAndProfile">
+                            <div className='headerBtns'>
+                                <button
+                                    className='btn btnSingIn'
+                                    style={{
+                                        display: isAuthenticated ? 'none' : ''
+                                    }}
+                                    onClick={() => navigate('/login')}
+                                >Login</button>
+                                <button
+                                    className='btn btnSingUp'
+                                    onClick={handleLogOut}
+                                >Log out</button>
                             </div>
-                        }
+                            {
+                                isAuthenticated && <div className="userProfile">
+                                    <img src="images/user.jpg" />
+                                    <p>{` Hi ${user.name}`}</p>
+                                </div>
+                            }
+                        </div>
+                    </div>
+
+                    <div className='menuIcon' onClick={() => setIsOpen(is => !is)}>
+                        <AiOutlineMenu />
                     </div>
 
                 </div>
